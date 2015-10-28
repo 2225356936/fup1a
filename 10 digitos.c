cc7b55171466958cba9bd0bd4f5430cbc2618f7f
paco <yugioh_geminis@hotmail.com>
2015-10-27 21:45:40 -0600

    tareas

---
+++ 10 digitos.c
@@ -0,0 +1,165 @@
+#include<stdio.h>
+int n1;
+int n2;
+int n3;
+int n4;
+int n5;
+int n6;
+int n7;
+int n8;
+int n9;
+int n10;
+int main()
+{
+
+
+   printf("ingrese n1:");
+   scanf("%d",&n1);
+   printf("ingrese n2:");
+   scanf("%d",&n2);
+   printf("ingrese n3:");
+   scanf("%d",&n3);
+   printf("ingrese n4:");
+   scanf("%d",&n4);
+   printf("ingrese n5:");
+   scanf("%d",&n5);
+   printf("ingrese n6:");
+   scanf("%d",&n6);
+   printf("ingrese n7:");
+   scanf("%d",&n7);
+   printf("ingrese n8:");
+   scanf("%d",&n8);
+   printf("ingrese n9:");
+   scanf("%d",&n9);
+   printf("ingrese n10:");
+   scanf("%d",&n10);
+
+
+if(n1>n2&&n1>n3&&n1>n4&&n1>n5){
+        if(n1>n6&&n1>n7&&n1>n8&&n1>n9&&n1>n10){
+
+
+        printf("el numero mayor es:%d\n",n1);
+     }
+}if(n2>n1&&n2>n3&&n2>n4&&n2>n5){
+        if(n2>n6&&n2>n7&&n2>n8&&n2>n9&&n2>n10){
+
+
+        printf("el numero mayor es:%d\n",n2);
+
+ }
+
+} if(n3>n2&&n3>n1&&n3>n4&&n3>n5){
+        if(n3>n6&&n3>n7&&n3>n8&&n3>n9&&n3>n10){
+
+
+        printf("el numero mayor es:%d\n",n3);
+
+ }
+} if(n4>n2&&n4>n3&&n4>n1&&n4>n5){
+        if(n4>n6&&n4>n7&&n4>n8&&n4>n9&&n4>n10){
+
+
+        printf("el numero mayor es:%d\n",n4);
+
+ }
+} if(n5>n2&&n5>n3&&n5>n4&&n5>n1){
+        if(n5>n6&&n5>n7&&n5>n8&&n5>n9&&n5>n10){
+
+
+        printf("el numero mayor es:%d\n",n5);
+
+ }
+} if(n6>n2&&n6>n3&&n6>n4&&n6>n5){
+        if(n6>n1&&n6>n7&&n6>n8&&n6>n9&&n6>n10){
+
+
+        printf("el numero mayor es:%d\n",n6);
+
+ }
+} if(n7>n2&&n7>n3&&n7>n4&&n7>n5){
+        if(n7>n6&&n7>n1&&n7>n8&&n7>n9&&n7>n10){
+
+
+        printf("el numero mayor es:%d\n",n7);
+
+ }
+}if(n8>n2&&n8>n3&&n8>n4&&n8>n5){
+        if(n8>n6&&n8>n7&&n8>n1&&n8>n9&&n8>n10){
+
+
+        printf("el numero mayor es:%d\n",n8);
+
+ }
+} if(n9>n2&&n9>n3&&n9>n4&&n9>n5){
+        if(n9>n6&&n9>n7&&n9>n8&&n9>n1&&n9>n10){
+
+
+        printf("el numero mayor es:%d\n",n9);
+
+  }
+ }if(n10>n2&&n10>n3&&n10>n4&&n10>n5){
+        if(n10>n6&&n10>n7&&n10>n8&&n10>n9&&n10>n1){
+
+
+        printf("el numero mayor es:%d\n",n10);
+
+  }
+}
+
+
+if(n1<n2&&n1<n3&&n1<n4&&n1<n5){
+        if(n1<n6&&n1<n7&&n1<n8&&n1<n9&&n1<n10){
+            printf("el numero menor es:%d",n1);
+
+	}
+} if(n2<n1&&n2<n3&&n2<n4&&n2<n5){
+        if(n2<n6&&n2<n7&&n2<n8&&n2<n9&&n2<n10){
+            printf("el numero menor es:%d",n2);
+
+	}
+} if(n3<n2&&n3<n1&&n3<n4&&n3<n5){
+        if(n3<n6&&n3<n7&&n3<n8&&n3<n9&&n3<n10){
+            printf("el numero menor es:%d",n3);
+
+	}
+} if(n4<n2&&n4<n3&&n4<n1&&n4<n5){
+        if(n4<n6&&n4<n7&&n4<n8&&n4<n9&&n4<n10){
+            printf("el numero menor es:%d",n4);
+
+	}
+} if(n5<n2&&n5<n3&&n5<n4&&n5<n1){
+        if(n5<n6&&n5<n7&&n5<n8&&n5<n9&&n5<n10){
+            printf("el numero menor es:%d",n5);
+
+	}
+} if(n6<n2&&n6<n3&&n6<n4&&n6<n5){
+        if(n6<n1&&n6<n7&&n6<n8&&n6<n9&&n6<n10){
+            printf("el numero menor es:%d",n6);
+
+	}
+} if(n7<n2&&n7<n3&&n7<n4&&n7<n5){
+        if(n7<n6&&n7<n1&&n7<n8&&n7<n9&&n7<n10){
+            printf("el numero menor es:%d",n7);
+
+	}
+} if(n8<n2&&n8<n3&&n8<n4&&n8<n5){
+        if(n8<n6&&n8<n7&&n8<n1&&n8<n9&&n8<n10){
+            printf("el numero menor es:%d",n8);
+
+	}
+}else if(n9<n2&&n9<n3&&n9<n4&&n9<n5){
+        if(n9<n6&&n9<n7&&n9<n8&&n9<n1&&n9<n10){
+            printf("el numero menor es:%d",n9);
+
+	}
+} if(n10<n2&&n10<n3&&n10<n4&&n10<n5){
+        if(n10<n6&&n10<n7&&n10<n8&&n10<n9&&n10<n1){
+            printf("el numero menor es:%d",n10);
+
+	}
+}
+
+		return 0;
+}
+
